33133e123795789bb205ada5c2f7d166acadf716
HarshAProgrammer <39998012+HarshAProgrammer@users.noreply.github.com>
2024-04-01 18:14:48 +0530

    first commit

---
+++ app/src/main/java/com/example/customdatabinding/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.customdatabinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.customdatabinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val post = Post("Introduction to Kotlin",
+            "Harsh Agarwal","")
+
+        binding.post = post
+
+    }
+}
